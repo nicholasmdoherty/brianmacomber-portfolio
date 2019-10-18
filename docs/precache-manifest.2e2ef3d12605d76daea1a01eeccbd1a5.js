9b177ef94d621f9b4d8e954c4a968402c641f8b2
nicholasmdoherty <nicholasmdoherty@gmail.com>
2019-10-18 17:30:03 -0400

    [deploy] deploy initial boilerplate to docs/

---
+++ docs/precache-manifest.2e2ef3d12605d76daea1a01eeccbd1a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c210ad6cba19f3b78279ac762d3dcf08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e08c3906621918e82cc",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f91690163ccbbeb4423d",
+    "url": "/static/js/2.ccd07799.chunk.js"
+  },
+  {
+    "revision": "0e08c3906621918e82cc",
+    "url": "/static/js/main.6aba3d40.chunk.js"
+  },
+  {
+    "revision": "d2897e4c8734fe48b145",
+    "url": "/static/js/runtime-main.262bc87e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
